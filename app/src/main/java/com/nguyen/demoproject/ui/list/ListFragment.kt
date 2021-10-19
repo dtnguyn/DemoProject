17b8be9c1f5d8aef10b05c8478ffbb4ce44eaf46
Adron Nguyen <adron0914@gmail.com>
2021-10-19 11:38:20 -0400

    Show error message with Toast

--- app/src/main/java/com/nguyen/demoproject/ui/list/ListFragment.kt
+++ app/src/main/java/com/nguyen/demoproject/ui/list/ListFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.nguyen.demoproject.R
@@ -42,7 +43,7 @@ class ListFragment : Fragment() {
 
                 }
                 is UIState.Error -> {
-
+                    Toast.makeText(requireContext(), "Error: ${state.errorMsg}", Toast.LENGTH_SHORT).show()
                 }
                 is UIState.Loaded -> {
                     state.value?.let{
