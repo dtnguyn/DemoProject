67e262da9565a13ca8188efa6e11b960b32ff7fb
Adron Nguyen <adron0914@gmail.com>
2021-10-18 22:40:40 -0400

    Initial setup

---
+++ app/src/main/java/com/nguyen/demoproject/ui/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.nguyen.demoproject.ui
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.nguyen.demoproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
