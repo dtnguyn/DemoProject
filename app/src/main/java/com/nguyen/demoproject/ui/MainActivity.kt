fe3f97f59628d73d4d8c877e92097db343f26f79
Adron Nguyen <adron0914@gmail.com>
2021-10-18 22:58:24 -0400

    Add list fragment

--- app/src/main/java/com/nguyen/demoproject/ui/MainActivity.kt
+++ app/src/main/java/com/nguyen/demoproject/ui/MainActivity.kt
@@ -7,13 +7,14 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.nguyen.demoproject.R
 import com.nguyen.demoproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_main)
     }
 
 }
